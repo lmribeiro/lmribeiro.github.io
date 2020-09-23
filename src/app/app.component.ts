490de454ae4c49fb3fb677c40bbbc694f5669770
Luís Ribeiro <luis.mribeiro@sapo.pt>
2020-09-23 23:32:16 +0100

    Update components style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,16 @@ export class AppComponent implements OnInit {
   class = 'on';
   tClass = 'nav-toggle';
   db = null;
-  firebaseConfig = {};
+  firebaseConfig = {
+    apiKey: 'AIzaSyCEQEiuAhXViTa0q-gXFSUkuJyEkdM0Iaw',
+    authDomain: 'lribeiro-js-org.firebaseapp.com',
+    databaseURL: 'https://lribeiro-js-org.firebaseio.com',
+    projectId: 'lribeiro-js-org',
+    storageBucket: 'lribeiro-js-org.appspot.com',
+    messagingSenderId: '29802766899',
+    appId: '1:29802766899:web:7a5783a6434e194da03946',
+    measurementId: 'G-7XHNTFSH45'
+  };
   educations = [];
   experiences = [];
 
